481a37915dd17b0a268bde3c09b0dd3b2bf161ec
Conan0122 <dmxking862@gmail.com>
2024-08-25 18:31:11 +0530

    Gone through useState in React
    -Finished a basic project to practice usecase of useState()

--- viteReactPrac/src/App.jsx
+++ viteReactPrac/src/App.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
+
+
 function App() {
+  let [counter, setCounter] = useState(0)
+
+  const addValue = ()=>{
+    console.log(`Value added ${counter}`);
+    if (counter==10){
+      alert('Cannot go above 10')
+    }
+    else{
+      setCounter(++counter)
+    }
+  }
+  const subValue = ()=>{
+    console.log(`Value subtracted ${counter}`);
+    if (counter==0){
+      alert('Cannot go below 0')
+    }
+    else{
+      setCounter(--counter)
+    }
+  }
+
   return (
-    <h1>Hello Conan</h1>
+    <>
+      <h1>Hello Conan! {counter}</h1>
+      <h1>Current value : {counter}</h1>
+      <button onClick={addValue}>Add</button>
+      <br />
+      <button onClick={subValue}>Subtract</button>
+      
+    </>
   )
 }
 
